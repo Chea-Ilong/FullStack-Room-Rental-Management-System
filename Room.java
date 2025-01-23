9a0f050e9d854855f8c4b80e76b0875545fc153f
Bunheang360 <2005chhengbunheang@gmail.com>
2025-01-24 00:21:07 +0700

    BUHAHAHA

---
+++ Room.java
@@ -0,0 +1,18 @@
+public class Room {
+    private String RoomID;
+    private double RoomPrice;
+    private int oldWaterCounter;
+    private int oldElectricCounter;
+    private int usedWater;
+    private int usedElectric;
+    private int newWaterCounter;
+    private int newElectricCounter;
+    private double electricPrice;
+    private double waterPrice;
+
+
+
+    public double CalculateWaterPrice () {
+        return waterPrice = (newWaterCounter - oldWaterCounter) * 2500;
+    }
+}
