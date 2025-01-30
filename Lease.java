bc1080d1f8decb862955cf60c77a65a25cce264a
Chea Ilong <155554832+Chea-Ilong@users.noreply.github.com>
2025-01-30 23:38:39 +0700

    fuck5

--- Lease.java
+++ Lease.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 public class Lease {
-    private int leaseCounter = 0; // Auto-generate lease IDs
+    private static int leaseCounter = 0;
     private int leaseId;
     private int tenantId;
     private int roomID;
@@ -10,15 +10,16 @@ public class Lease {
     private double monthlyRent;
     private String leaseStatus;
 
+
+
     public Lease(int tenantId, int roomID, LocalDate startDate, LocalDate endDate, double monthlyRent) {
 
-        leaseCounter++;
-        this.leaseId = leaseCounter;
+        this.leaseId = ++leaseCounter;
         this.tenantId = tenantId;
         this.roomID = roomID;
         this.startDate = startDate;
         this.endDate = endDate;
         this.monthlyRent = monthlyRent;
-        this.leaseStatus = "Active"; // Default status
+        this.leaseStatus = "Active";
     }
 }
--- Room.java
+++ Room.java
@@ -3,16 +3,16 @@ public class Room {
     private String roomID;
     private double roomPrice;
     private String roomType;
-    private boolean status ;
-    private int waterCounterUsage;
-    private int electricCounterUsage;
-    private int currentElectricCounter;
-    private int currentWaterCounter;
-    private double electricPrice = 620;
-    private double waterPrice = 2500;
+    private boolean status;
+    private int waterCounterUsage ;
+    private int electricCounterUsage ;
+    private int currentElectricCounter ;
+    private int currentWaterCounter ;
+    private final int electricPrice = 620;
+    private final int waterPrice = 2500;
 
     public Room(String roomID, double roomPrice, String roomType, boolean status) {
-        // Required fields
+
         this.roomID = roomID;
         this.roomPrice = roomPrice;
         this.roomType = roomType;
@@ -20,8 +20,5 @@ public class Room {
 
     }
 
-
-    //    public void CalculateWaterPrice(){};
-//    public void CalculateElectricPrice(){};
 
 }
