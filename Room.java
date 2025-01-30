7323f50cc68adfa1ea11a1d43dee7740b4400ce1
Chea Ilong <155554832+Chea-Ilong@users.noreply.github.com>
2025-01-30 12:49:48 +0700

    fuck2

--- Room.java
+++ Room.java
@@ -1,16 +1,30 @@
 public class Room {
-    private String RoomID;
-    private double RoomPrice;
-    private int oldWaterCounter;
-    private int oldElectricCounter;
-    private int usedWater;
-    private int usedElectric;
-    private int newWaterCounter;
-    private int newElectricCounter;
-    private double electricPrice;
-    private double waterPrice;
 
-    public double CalculateWaterPrice () {
-        return waterPrice = (newWaterCounter - oldWaterCounter) * 2500;
+    private String roomID;
+    private double roomPrice;
+    private String roomType;
+    private boolean status ;
+    private int waterCounterUsage;
+    private int electricCounterUsage;
+    private int currentElectricCounter;
+    private int currentWaterCounter;
+    private double electricPrice = 620;
+
+
+    private double waterPrice = 2500;
+
+
+
+    public Room(String roomID, double roomPrice, String roomType, boolean status, int waterCounterUsage, int electricCounterUsage) {
+        this.roomID = roomID;
+        this.roomPrice = roomPrice;
+        this.roomType = roomType;
+        this.status = status;
+        this.waterCounterUsage = waterCounterUsage;
+        this.electricCounterUsage = electricCounterUsage;
     }
+
+    //    public void CalculateWaterPrice(){};
+//    public void CalculateElectricPrice(){};
+
 }
