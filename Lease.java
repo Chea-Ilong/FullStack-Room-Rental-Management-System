ac8c3c9c9b86fcb1988968e56d2647b1a809a626
Chea Ilong <155554832+Chea-Ilong@users.noreply.github.com>
2025-01-30 14:38:30 +0700

    fuck4

--- Lease.java
+++ Lease.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 
 public class Lease {
+    private int leaseCounter = 0; // Auto-generate lease IDs
     private int leaseId;
     private int tenantId;
     private int roomID;
@@ -9,12 +10,15 @@ public class Lease {
     private double monthlyRent;
     private String leaseStatus;
 
-
     public Lease(int tenantId, int roomID, LocalDate startDate, LocalDate endDate, double monthlyRent) {
+
+        leaseCounter++;
+        this.leaseId = leaseCounter;
         this.tenantId = tenantId;
         this.roomID = roomID;
         this.startDate = startDate;
         this.endDate = endDate;
         this.monthlyRent = monthlyRent;
+        this.leaseStatus = "Active"; // Default status
     }
 }
--- Room.java
+++ Room.java
@@ -9,21 +9,17 @@ public class Room {
     private int currentElectricCounter;
     private int currentWaterCounter;
     private double electricPrice = 620;
-
-
     private double waterPrice = 2500;
 
-
-
-
-    public Room(String roomID, double roomPrice, String roomType, boolean status, int waterCounterUsage, int electricCounterUsage) {
+    public Room(String roomID, double roomPrice, String roomType, boolean status) {
+        // Required fields
         this.roomID = roomID;
         this.roomPrice = roomPrice;
         this.roomType = roomType;
         this.status = status;
-        this.waterCounterUsage = waterCounterUsage;
-        this.electricCounterUsage = electricCounterUsage;
+
     }
+
 
     //    public void CalculateWaterPrice(){};
 //    public void CalculateElectricPrice(){};
