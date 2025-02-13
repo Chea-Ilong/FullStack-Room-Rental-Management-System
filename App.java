89422a150dc2b4584b25e201626066704978f188
nithkidd <123559135+nithkidd@users.noreply.github.com>
2025-02-13 16:12:29 +0700

    Tenant

--- App.java
+++ App.java
@@ -9,18 +9,39 @@ public class App {
 //        room3.displayRoomInfo();
 //        room43.displayRoomInfo();
 //
-        Tenant tenant2 = new Tenant("long", "555-1234", 111, true);
         Tenant tenant1 = new Tenant("John Doe", "555-1234", 101, true);
+        Tenant tenant2 = new Tenant("long", "555-1234", 111, true);
         tenant1.signUp();
         tenant2.signUp();
-        tenant2.login("John Doe",101);
-        tenant1.login("long",111);
+        // tenant2.login("John Doe",101);
+        // tenant1.login("long",111);
+        tenant1.login("John Doe",101);
+        tenant2.login("long",111);
 //
 //        Tenant tenant4 = new Tenant("io right", "5555-1234", 1011, true);
 //        tenant4.signUp();
 //        tenant4.login();
 
 
+        // Checking payment status before paying
+        System.out.println("Username: " + tenant1.getTenantName()); // Output: "Rent is NOT paid."
+        System.out.println(tenant1.checkPaymentStatus()); // Output: "Rent is NOT paid."
+
+        // Paying rent
+        tenant1.payRent();
+
+        // Checking payment status after paying
+        System.out.println(tenant1.checkPaymentStatus()); // Output: "Rent is paid. Last payment: [date]"
+
+        // Checking payment status before paying
+        System.out.println("Username: " + tenant2.getTenantName()); // Output: "Rent is NOT paid."
+        System.out.println(tenant2.checkPaymentStatus()); // Output: "Rent is NOT paid."
+
+        // Paying rent
+        tenant2.payRent();
+
+        // Checking payment status after paying
+        System.out.println(tenant2.checkPaymentStatus()); // Output: "Rent is paid. Last payment: [date]"
 //
 //        // Creating issue reports
 //        ReportIssue issue1 = new ReportIssue("A101", "John Doe", "T12345", "Water leakage in bathroom.");
